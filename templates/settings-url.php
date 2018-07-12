84c74f558d8d25c96931da4d76268aaa7c7623da
Warwick Booth <warwick@lsdev.biz>
2018-07-12 12:16:54 +0200

    Setting the defaul value for the oAuth URL

--- templates/settings-url.php
+++ templates/settings-url.php
@@ -1,3 +1,8 @@
+<?php
+if ( '' === $value ) {
+	$value = 'https://accounts.zoho.com/oauth/v2';
+} ?>
+
 <input id="<?php echo $id; ?>" name="<?php echo $name; ?>" class="regular-text" value="<?php echo $value; ?>" placeholder="https://accounts.zoho.com/oauth/v2" />
 
 <p class="description">
