2b41f84565a8795210c5b95887fb5f92a8c009d7
Warwick Booth <warwick@lsdev.biz>
2019-01-16 05:59:46 +0200

    Removing the old references

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,7 @@ gulp.task('default', function() {
 	console.log('Use the following commands');
 	console.log('--------------------------');
 	console.log('gulp compile-js                to compile the js to min.js');
-	console.log('gulp wordpress-lang            to compile the tour-operator.pot, tour-operator-en_EN.po and tour-operator-en_EN.mo');
-});
-
-gulp.task('watch-css', function () {
-	return gulp.watch('assets/css/**/*.scss', ['compile-css']);
-});
-
-gulp.task('watch-js', function () {
-	return gulp.watch('assets/js/src/**/*.js', ['compile-js']);
+	console.log('gulp wordpress-lang            to compile the lsx-cf-zoho.pot, lsx-cf-zoho-en_EN.po and lsx-cf-zoho-en_EN.mo');
 });
 
 /*  LANGUAGE SETTINGS */
@@ -23,8 +15,8 @@ gulp.task('wordpress-pot', function() {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
-			domain: 'tour-operator',
-			package: 'tour-operator',
+			domain: 'lsx-cf-zoho',
+			package: 'lsx-cf-zoho',
 			bugReport: 'https://github.com/lightspeeddevelopment/cf-zoho/issues',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
@@ -35,8 +27,8 @@ gulp.task('wordpress-po', function() {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
-			domain: 'tour-operator',
-			package: 'tour-operator',
+			domain: 'lsx-cf-zoho',
+			package: 'lsx-cf-zoho',
 			bugReport: 'https://github.com/lightspeeddevelopment/cf-zoho/issues',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
